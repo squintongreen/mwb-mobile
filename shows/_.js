0a96a161cde4af5bbaac80e3b674687bd09d309f
Arsen <gutsal.arsen@softsky.com.ua>
2012-04-14 17:11:54 +0300

    first commit

---
+++ shows/_.js
@@ -0,0 +1,22 @@
+function(doc, req) {
+    // !json template
+    // !code vendor/couchapp/lib/underscore.js
+
+    _ = this._
+    // Enables Mustache.js-like templating.    
+    /*
+     _.templateSettings = {
+         interpolate : /\{\{(.+?)\}\}/g
+     };
+     */
+
+    template = template[doc.template || '1']
+
+    
+    processTemplate = function(fileName){
+        var callback = _.template(template[fileName])
+        return callback({doc: doc, req: req, template: template, processTemplate: processTemplate })
+    }
+    //send(JSON.stringify(doc))
+    send(processTemplate(req.query.key.replace(/\.html/g,'')))
+}
